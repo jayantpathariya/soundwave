ccfaad001c2e2c9a65ee544fc176afbcb46eb187
Jayant Pathariya <jayantmehra02@gmail.com>
2024-09-10 18:34:38 +0530

    play from track item added

--- components/track-item.tsx
+++ components/track-item.tsx
@@ -1,10 +1,15 @@
 import { decode } from "html-entities";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import FastImage from "react-native-fast-image";
+import LoaderKit from "react-native-loader-kit";
+import TrackPlayer, {
+  useActiveTrack,
+  useIsPlaying,
+} from "react-native-track-player";
 
 import { unknownTrackImageUrl } from "@/constants/images";
 import { colors, fontSizes } from "@/constants/tokens";
-import { wp } from "@/lib/utils";
+import { createTrack, wp } from "@/lib/utils";
 import { Song } from "@/types/song";
 import { Ionicons } from "@expo/vector-icons";
 import { MovingText } from "./moving-text";
@@ -14,13 +19,44 @@ type TrackItemProps = {
 };
 
 export function TrackItem({ track }: TrackItemProps) {
+  const { playing } = useIsPlaying();
+  const isActiveTrack = useActiveTrack()?.id === track.id;
+
+  console.log({ isActiveTrack });
+
+  const handlePlayTrack = async () => {
+    await TrackPlayer.add([createTrack(track)]);
+    await TrackPlayer.play();
+  };
+
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.7}>
+    <TouchableOpacity
+      style={styles.container}
+      activeOpacity={0.7}
+      onPress={handlePlayTrack}
+    >
       <View style={styles.infoContainer}>
-        <FastImage
-          source={{ uri: track.image[1].url ?? unknownTrackImageUrl }}
-          style={styles.image}
-        />
+        <View>
+          <FastImage
+            source={{ uri: track.image[1].url ?? unknownTrackImageUrl }}
+            style={[styles.image, { opacity: isActiveTrack ? 0.6 : 1 }]}
+          />
+          {isActiveTrack &&
+            (playing ? (
+              <LoaderKit
+                name="LineScaleParty"
+                color={colors.icon.primary}
+                style={styles.trackPlayingIcon}
+              />
+            ) : (
+              <Ionicons
+                name="play"
+                size={24}
+                color={colors.icon.primary}
+                style={styles.trackPausedIcon}
+              />
+            ))}
+        </View>
         <View style={styles.textContainer}>
           <MovingText
             style={styles.title}
@@ -58,6 +94,18 @@ const styles = StyleSheet.create({
     width: wp(14),
     aspectRatio: 1,
     borderRadius: wp(1),
+  },
+  trackPausedIcon: {
+    position: "absolute",
+    top: wp(3.5),
+    left: wp(4),
+  },
+  trackPlayingIcon: {
+    width: wp(5),
+    aspectRatio: 1,
+    position: "absolute",
+    top: wp(4.5),
+    left: wp(4),
   },
   textContainer: {
     flex: 1,
